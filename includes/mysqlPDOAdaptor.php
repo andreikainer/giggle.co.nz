e867e0a1cf3ef97c6f82196cc9b2fe686829e208
Andrei Kainer <andreikainer@gmail.com>
2015-02-26 13:57:24 +1300

    database connectivity

--- includes/mysqlPDOAdaptor.php
+++ includes/mysqlPDOAdaptor.php
@@ -1,40 +1,40 @@
 <?php
-define("HOST", "mysql5.openhost.net.nz");
-define("USER_NAME", "dreig_andrei");
-define("PASSWORD", "yoobee2015!$");
-define("DB_NAME", "dreigma30401com38097_giggle");
+define("HOST", "");
+define("USER_NAME", "");
+define("PASSWORD", "");
+define("DB_NAME", "");
 
 require_once('class.tools.php');
 
-class Database{                                                         
+class Database{
                                                             // the class Database is used to connect and execute the database queries
-	private $PDOConnection;                                    // set the class properties 
-    private static $dbInstance = null; 
-    
+	private $PDOConnection;                                    // set the class properties
+    private static $dbInstance = null;
+
     private function __construct()
     {
-		try 
+		try
 		{
 	     $this->PDOConnection = new PDO('mysql:host='.HOST.';dbname='.DB_NAME, USER_NAME, PASSWORD); // connection info
 		 $this->PDOConnection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);             // set the PDO ERROR MODE
 	    }
-		catch(PDOException $e) 
+		catch(PDOException $e)
 		{
 		 echo 'Connection error: ' . $e->getMessage();
   	    }
-    } 
+    }
 
     public static function getInstance()                    // this function makes sure that there is only one instance of a class: http://stackoverflow.com/questions/1449362/origin-explanation-of-classgetinstance
-    { 
+    {
         if (is_null(self::$dbInstance))                     // if no instance exists then create one
-        { 
-            self::$dbInstance = new Database(); 
+        {
+            self::$dbInstance = new Database();
         }
         return self::$dbInstance;                           // self is like $this but used specific for classes to instantiate a new class
     }
-    
 
-    
+
+
     public function close_connection()                      // function closes the PDO connection
     {
         $this->PDOConnection = null;
@@ -45,23 +45,23 @@ class Database{
     public function query($sSQL)                                // function queries the datebase, if there is an error echo it out
     {
         // echo $sSQL.'<br/>';
-        try 
+        try
         {
             $resResult = $this->PDOConnection->query($sSQL);    // return the results of the query, or 'false' on failure
             return $resResult;                                  // http://php.net/manual/en/pdo.query.php
-        } 
+        }
         catch(PDOException $e) {
             echo 'ERROR: ' . $e->getMessage();                  // display the error message if the function execution fails
         }
-        
+
     }
-    
+
 
     public function fetch_array($resResult)                     // function fetches the data as an associative array
     {
         return $resResult->fetch(PDO::FETCH_ASSOC);
     }
-    
+
 
 
     public function num_rows($resResult)                        // function fetches the data as an numeric array
@@ -75,13 +75,7 @@ class Database{
     {
         return $this->PDOConnection->lastInsertId();
     }
-	
+
 } // EOF Class Database
 
 ?>
-
-
-
-
-
-
--- includes/settings.ini.php
+++ includes/settings.ini.php
@@ -1,6 +1,6 @@
 ;<?php return; ?>
 [SQL]
-host = mysql5.openhost.net.nz
-user = dreig_andrei
-password = yoobee2015!$
-dbname = dreigma30401com38097_giggle
+host =
+user =
+password =
+dbname = 
